328191a850a283d430a70ddebf3d76863a8eb138
jackkslash <jackfaulknerbis@hotmail.co.uk>
2025-01-18 19:36:39 +0000

    Basic chatbot implemented

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,32 @@
+import { google } from '@ai-sdk/google';
+import { streamText, tool } from 'ai';
+import { z } from 'zod';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+    const { messages } = await req.json();
+
+    const result = streamText({
+        model: google('gemini-1.0-pro'),
+        messages,
+        tools: {
+            weather: tool({
+                description: 'Get the weather in a location (fahrenheit)',
+                parameters: z.object({
+                    location: z.string().describe('The location to get the weather for'),
+                }),
+                execute: async ({ location }) => {
+                    const temperature = Math.round(Math.random() * (90 - 32) + 32);
+                    return {
+                        location,
+                        temperature,
+                    };
+                },
+            }),
+        },
+    });
+
+    return result.toDataStreamResponse();
+}
